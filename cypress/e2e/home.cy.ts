2aa7a819c93e3bfe5ee62b13bfe3df5aa2216755
Ro008 <rociosirvent@gmail.com>
2023-04-14 20:16:07 +0300

    use context for better test organization and creation of user journey spec file

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,12 +3,33 @@ describe('home page', () => {
     cy.visit('http://localhost:3000/')
   })
 
-  it('the h1 contains the correct text', () => {
-    cy.getByData('hero-heading').contains('Testing Next.js Applications with Cypress');
+  context('hero section', () => {
+    it('the h1 contains the correct text', () => {
+      cy.getByData('hero-heading').contains('Testing Next.js Applications with Cypress');
+    })
+
+    it('the features on the homepage are correct', () => {
+      cy.get('dt').eq(0).contains('4 Courses');
+      cy.get('dt').eq(1).contains('25+ Lessons');
+    })
   })
 
-  it('the features on the homepage are correct', () => {
-    cy.get('dt').eq(0).contains('4 Courses');
-    cy.get('dt').eq(1).contains('25+ Lessons');
+  context('courses section', () => {
+    it('course: Testing Your First Next.js Application', () => {
+      cy.getByData('course-0').find('a').eq(3).click();
+      cy.location('pathname').should('eq', '/testing-your-first-application')
+    })
+
+    it('course: Testing Foundations', () => {
+      cy.getByData('course-1').find('a').eq(3).click();
+      cy.location('pathname').should('eq', '/testing-foundations')
+    })    
+
+    it('course: Cypress Fundamentals', () => {
+      cy.getByData('course-2').find('a').eq(3).click();
+      cy.location('pathname').should('eq', '/cypress-fundamentals')
+    })
+
   })
+
 })
